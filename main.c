33ac081c18a0f15fd83e51161d6ba2de9d62fc13
CamiloHurtado99 <95389919+Jc99h@users.noreply.github.com>
2021-12-10 01:03:44 +0000

    Finished function fork()

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main(int argc, char *argv[])
+{
+  int contador=1;
+  int rc = fork();
+
+  if(rc < 0)
+  {
+    fprintf(stderr, "fork failed\n");
+  }
+  else
+  {
+    if(rc==0) //is the child
+    {
+      contador+=5;
+      printf("Soy el hijo (pid:%d) y el valor de mi contador es: %i\n", getpid(), contador);
+    }
+    else //is the parent
+    {
+      contador+=2;
+      printf("Soy el padre de %d (pid:%d) y el valor de mi contador es: %i\n", rc, getpid(), contador);
+    }
+  }
+  printf("Al final antes del return del programa el contador imprime: %i", contador);
   return 0;
 }
